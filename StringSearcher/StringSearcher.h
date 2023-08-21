add12b73f1cb0876403826930eafaac251acb571
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2023-08-21 16:39:56 +0200

    update

--- StringSearcher/StringSearcher.h
+++ StringSearcher/StringSearcher.h
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <assert.h>
 #include <filesystem>
 #include <fstream>
 #include <functional>
@@ -94,15 +95,21 @@ namespace RDW_SS
 			}
 		}
 
-		inline static void IsStringInFile() {}
-
 		inline static std::vector<std::string> GetAllFilesInDirectory(const std::string& rootDir, const std::string& fileToLookThrough)
 		{
 			const std::filesystem::path directory{ rootDir };
 			std::vector<std::string> fileNames{};
 
 			// reserve some memory for all of the files/directories in the directory
-			fileNames.reserve(std::distance(std::filesystem::directory_iterator(directory), std::filesystem::directory_iterator()));
+			fileNames.reserve(50);
+
+			std::string filename{};
+			std::string extension{};
+			if (!fileToLookThrough.empty())
+			{
+				filename = fileToLookThrough.substr(0, fileToLookThrough.find("."));
+				extension = fileToLookThrough.substr(fileToLookThrough.find("."));
+			}
 
 			for (const std::filesystem::path& path : std::filesystem::directory_iterator(directory))
 			{
@@ -113,14 +120,8 @@ namespace RDW_SS
 				}
 				else if (std::filesystem::is_regular_file(path))
 				{
-					if (!fileToLookThrough.empty())
-					{
-						const std::string filename{ fileToLookThrough.substr(0, fileToLookThrough.find(".")) };
-						const std::string extension{ fileToLookThrough.substr(fileToLookThrough.find(".")) };
-
-						if (filename != "*" && filename != path.filename().string()) continue;
-						if (extension != "*" && extension != path.extension().string()) continue;
-					}
+					if (filename != "*" && filename != path.filename().string()) continue;
+					if (extension != "*" && extension != path.extension().string()) continue;
 
 					fileNames.emplace_back(path.string());
 				}
@@ -139,18 +140,44 @@ namespace RDW_SS
 			const size_t nrOfThreads{ std::thread::hardware_concurrency() };
 			const std::vector<std::string> allFiles{ Detail::GetAllFilesInDirectory(currentDir, fileToLookThrough) };
 			const size_t nrOfFilesPerThread{ allFiles.size() / nrOfThreads };
+
+			if (nrOfFilesPerThread == 0) return;
+
+			const bool evenNumberOfFiles{ allFiles.size() % nrOfThreads == 0 };
 			std::vector<std::thread> threads{};
 			threads.reserve(nrOfThreads);
 			std::mutex mutex{};
 
-			if (nrOfFilesPerThread == 0) return;
-
 			using FuncType = void(const std::vector<std::string>&, std::string, const bool, std::unordered_map<std::string, std::vector<uint32_t>>&, std::mutex&);
 
-			for (size_t i{}; i < nrOfThreads; ++i)
+#ifdef _DEBUG
+			size_t counter = 0;
+#endif
+
+			for (size_t i{}; i < nrOfThreads - 1; ++i)
 			{
-				threads.emplace_back(static_cast<FuncType*>(&Detail::IsStringInFile), std::vector<std::string>{"a"}, stringToSearch, ignoreCase, std::ref(foundStrings), std::ref(mutex));
+				const size_t startOffset{ nrOfFilesPerThread * i };
+				const size_t endOffset{ nrOfFilesPerThread * (i + 1) };
+
+#ifdef _DEBUG
+				counter += endOffset - startOffset;
+
+				{ // destroys debug performance but we dont care l o l
+					auto testFileSizes = std::vector<std::string>{ allFiles.begin() + startOffset, allFiles.begin() + endOffset };
+					assert(testFileSizes.size() == nrOfFilesPerThread);
+				}
+#endif
+
+				threads.emplace_back(static_cast<FuncType*>(&Detail::IsStringInFile), std::vector<std::string>{ allFiles.begin() + startOffset, allFiles.begin() + endOffset }, stringToSearch, ignoreCase, std::ref(foundStrings), std::ref(mutex));
 			}
+
+#ifdef _DEBUG
+			if (evenNumberOfFiles) assert(counter == allFiles.size() - nrOfFilesPerThread);
+			else assert(counter == allFiles.size() - nrOfFilesPerThread - 1);
+#endif
+
+			if (evenNumberOfFiles) threads.emplace_back(static_cast<FuncType*>(&Detail::IsStringInFile), std::vector<std::string>{ allFiles.begin() + allFiles.size() - nrOfFilesPerThread, allFiles.end() }, stringToSearch, ignoreCase, std::ref(foundStrings), std::ref(mutex));
+			else threads.emplace_back(static_cast<FuncType*>(&Detail::IsStringInFile), std::vector<std::string>{ allFiles.begin() + allFiles.size() - nrOfFilesPerThread - 1, allFiles.end() }, stringToSearch, ignoreCase, std::ref(foundStrings), std::ref(mutex));
 
 			for (size_t i{}; i < nrOfThreads; ++i) threads[i].join();
 		}
@@ -167,6 +194,7 @@ namespace RDW_SS
 		std::cout << "/i	ignore case of characters\n";
 		std::cout << "/c	string to search for (required)\n";
 		std::cout << "/f	files to look through (required if /s is not specified)\n";
+		std::cout << "Example: StringSearcher.exe /s /c Hello World! /f *.txt\n";
 	}
 }
 
